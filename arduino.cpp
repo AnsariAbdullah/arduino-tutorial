8bc16ec7a4ae45e3ea0c884eaa9610e988aef6f8
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-09-26 13:51:48 +0530

    print text in serial monitor on LED blink

--- arduino.cpp
+++ arduino.cpp
@@ -3,12 +3,17 @@
 void setup()
 {
   pinMode(13, OUTPUT);
+	Serial.begin(9600);
+  Serial.println("Hello Arduino");
 }
 
 void loop()
 {
+	Serial.println("LED on");
   digitalWrite(13, HIGH);
+
   delay(1000); // Wait for 1000 millisecond(s)
+  Serial.println("LED off");
   digitalWrite(13, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
