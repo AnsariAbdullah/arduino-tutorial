4c2fe630799195190158c67d862b1c7a073919d6
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-09-28 14:36:23 +0530

    variables, loops and array

--- arduino.cpp
+++ arduino.cpp
@@ -1,10 +1,30 @@
+// defining a constant variable
 #define INTERNAL_LED 13
+
+// defining a varibale
 int EXT_LED = 12;
+
+// defining an Array
+int temp[3] = {21, 22, 23};
+// 3 here defined the size of the array
 
 void setup()
 {
   pinMode(INTERNAL_LED, OUTPUT);
   pinMode(EXT_LED, OUTPUT);
+	Serial.begin(9600);
+	
+	// accessing value of array
+	Serial.println(temp[1]);
+
+	// changing the value at an index
+	temp[1] = 30;
+	Serial.println(temp[1]);
+
+	// using for loop to print all items of array
+	for(int i=0; i<3; i++){
+    Serial.println(temp[i]);
+  }
 }
 
 void loop()
