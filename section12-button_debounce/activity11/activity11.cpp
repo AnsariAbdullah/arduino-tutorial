caf5f65ca0f11b038381d67663690101014724c9
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-04 17:39:47 +0530

    optimize solution

--- section12-button_debounce/activity11/activity11.cpp
+++ section12-button_debounce/activity11/activity11.cpp
@@ -5,14 +5,11 @@
 
 unsigned long ledBlinkTime = millis();
 unsigned long blinkWait = 1000;
-int greenLEDState = LOW;
-
 unsigned long lastButtonStateChange = millis();
 unsigned long debounceTime = 50;
 byte buttonState = LOW;
 
-int toggleLEDState = 1;
-
+int greenLEDState = LOW;
 
 int RED_LIGHT = HIGH;
 int YELL_LIGHT = LOW;
@@ -23,8 +20,31 @@ void setup()
   pinMode(YELL_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
-	buttonState = digitalRead(BUTTON_PIN);
   digitalWrite(RED_LED, RED_LIGHT);
+}
+
+void blinkLed (){
+  if(greenLEDState == LOW){
+    greenLEDState = HIGH;
+  }else{
+    greenLEDState = LOW;
+  }
+  digitalWrite(GREEN_LED, greenLEDState);
+}
+
+void toggleLEDs(){
+  if(buttonState == HIGH){
+    RED_LIGHT = LOW;
+    YELL_LIGHT = HIGH;
+    digitalWrite(RED_LED, RED_LIGHT);
+    digitalWrite(YELL_LED, YELL_LIGHT);
+  }else{
+    RED_LIGHT = HIGH;
+    YELL_LIGHT = LOW;
+    digitalWrite(RED_LED, RED_LIGHT);
+    digitalWrite(YELL_LED, YELL_LIGHT);
+
+  }
 }
 
 void loop()
@@ -34,37 +54,16 @@ void loop()
   //make LED blink every 1000 ms (1 sec)
   if(timeNow - ledBlinkTime >= blinkWait){
   	ledBlinkTime += blinkWait;
-    if(greenLEDState == LOW){
-      greenLEDState = HIGH;
-    }else{
-      greenLEDState = LOW;
-    }
-    digitalWrite(GREEN_LED, greenLEDState);
+    blinkLed();
   }
   
   //toggle LEDs on button press
   if(timeNow - lastButtonStateChange > debounceTime){
   	byte newButtonState = digitalRead(BUTTON_PIN);
     if(newButtonState != buttonState){
-      lastButtonStateChange = timeNow;
+      lastButtonStateChange+= timeNow;
       buttonState = newButtonState;
-      if(buttonState == HIGH){
-				if(toggleLEDState == 1){
-					toggleLEDState = 2;
-					digitalWrite(RED_LED, LOW);
-        	digitalWrite(YELL_LED, HIGH);
-				}else{
-					toggleLEDState = 1;
-					digitalWrite(RED_LED, HIGH);
-        	digitalWrite(YELL_LED, LOW);
-				}        
-      }else{
-        RED_LIGHT = HIGH;
-        YELL_LIGHT = LOW;
-        digitalWrite(RED_LED, RED_LIGHT);
-        digitalWrite(YELL_LED, YELL_LIGHT);
-        
-      }
+      toggleLEDs();
     }
   } 
 }
