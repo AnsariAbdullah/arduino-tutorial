989ce47c561b7e432787cbe4a3d7d368af45b25f
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-12-07 17:55:47 +0530

    refactor and add complimentory filter

--- section16-lcd_screen/activity17/activity17.cpp
+++ section16-lcd_screen/activity17/activity17.cpp
@@ -1,28 +1,31 @@
 #include <LiquidCrystal.h>
 
 // Ultrasonic sensor pins
+#define LCD_RS_PIN A5
+#define LCD_E_PIN A4
+#define LCD_D4_PIN 6
+#define LCD_D5_PIN 7
+#define LCD_D6_PIN 8
+#define LCD_D7_PIN 9
+
+// LCD pins
 #define ECHO_PIN 3
 #define TRIGGER_PIN 4
 
-// LCD pins 
-#define LCD_RS_PIN A5
-#define LCD_E_PIN A4
-#define LCD_DB4_PIN 6
-#define LCD_DB5_PIN 7
-#define LCD_DB6_PIN 8
-#define LCD_DB7_PIN 9
+LiquidCrystal lcd(LCD_RS_PIN, LCD_E_PIN, LCD_D4_PIN,
+                  LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
-unsigned long lastTimeSensorTriggered = millis();
-unsigned long sensorTriggerDelay = 60;
+unsigned long lastTimeUltrasonicTrigger = millis();
+unsigned long ultrasonicTriggerDelay = 60;
 
 volatile unsigned long pulseInTimeBegin;
 volatile unsigned long pulseInTimeEnd;
-volatile bool newDistance = false;
+volatile bool newDistanceAvailable = false;
 
-LiquidCrystal lcd(LCD_RS_PIN, LCD_E_PIN, LCD_DB4_PIN,
-                 LCD_DB5_PIN, LCD_DB6_PIN, LCD_DB7_PIN);
+double previousDistance = 400.0;
 
-void triggerSensor() {
+void triggerUltrasonicSensor()
+{
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
@@ -30,54 +33,62 @@ void triggerSensor() {
   digitalWrite(TRIGGER_PIN, LOW);
 }
 
-double getUltrasonicDistance() {
+double getUltrasonicDistance()
+{
   double durationMicros = pulseInTimeEnd - pulseInTimeBegin;
-  double distance = durationMicros / 58.0;
+  double distance = durationMicros / 58.0; // cm (148.0: inches)
+  if (distance > 400.0) {
+    return previousDistance;
+  }
+  distance = previousDistance * 0.7 + distance * 0.3; //complimentary filter
+  previousDistance = distance;
   return distance;
 }
 
-void echoPointInterrupt(){
-  if(digitalRead(ECHO_PIN) == HIGH){ //start measuring
-  	pulseInTimeBegin = micros();
-  }else{ //stop measuring
-  	pulseInTimeEnd = micros();
-    newDistance = true;
+void echoPinInterrupt()
+{
+  if (digitalRead(ECHO_PIN) == HIGH) { // start measuring
+    pulseInTimeBegin = micros();
+  }
+  else { // stop measuring
+    pulseInTimeEnd = micros();
+    newDistanceAvailable = true;
   }
 }
 
-//lcd.setCursor(0, 1);
-
-void printDistanceOnLCD(double distance){
-  lcd.setCursor(0,1);
-  lcd.print("Distance :");
+void printDistanceOnLCD(double distance)
+{
+  lcd.setCursor(0, 1);
+  lcd.print("Distance: ");
   lcd.print(distance);
   lcd.print("  ");
 }
 
 void setup() {
-  lcd.begin(16, 2);
-  lcd.print("Rate ");
-  lcd.print(sensorTriggerDelay);
-  lcd.print(" ms");
-  
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Rate: ");
+  lcd.print(ultrasonicTriggerDelay);
+  lcd.print(" ms.");
+
   attachInterrupt(digitalPinToInterrupt(ECHO_PIN),
-                 echoPointInterrupt,
-                 CHANGE);
+                  echoPinInterrupt,
+                  CHANGE);
 }
 
 void loop() {
   unsigned long timeNow = millis();
-  if(timeNow - lastTimeSensorTriggered > sensorTriggerDelay){
-  	lastTimeSensorTriggered += timeNow;
-    triggerSensor();
-  }
   
-  if(newDistance){
-    newDistance = false;
+  if (timeNow - lastTimeUltrasonicTrigger > ultrasonicTriggerDelay) {
+    lastTimeUltrasonicTrigger += ultrasonicTriggerDelay;
+    triggerUltrasonicSensor();
+  }
+
+  if (newDistanceAvailable) {
+    newDistanceAvailable = false;
     double distance = getUltrasonicDistance();
     printDistanceOnLCD(distance);
   }
-  
 }
